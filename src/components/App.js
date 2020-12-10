dfe920241c99932c07b5e55c17d6cd5e28652d33
Aharon Lubitsch <59685123+koftov@users.noreply.github.com>
2020-12-10 18:43:52 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import useVideos from '../hooks/useVideos';
 
 const App = () => {
   const [selectedVideo, setSelectedVideo] = useState(null);
-  const [videos, search] = useVideos('טברנק');
+  const [videos, search] = useVideos('punk');
 
   useEffect(() => {
     setSelectedVideo(videos[0]);
